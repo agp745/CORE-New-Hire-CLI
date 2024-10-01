c5b2ce5d216ed9511943ca6450d4abc6422e4caa
agp745 <agperez745@gmail.com>
2024-10-01 09:06:28 -0500

    web request

--- install_script.ps1
+++ install_script.ps1
@@ -1,8 +1,11 @@
-# $CLI_URL = ""
-# $DestinationPath = "$HOME\Documents\WindowsPowerShell\Scripts\newHire.ps1"
-#
-# jf (-not (Test-Path $DestinationPath)) {
-#     New-Item -ItemType Directory -Path $DestinationPath
-# }
+$SourceURL = "https://raw.githubusercontent.com/agp745/CORE-New-Hire-CLI/refs/heads/main/newHire.ps1"
+$DestinationURL = "$HOME\Documents\scripts\newHireCLI.ps1"
 
-Write-Host -ForegroundColor Green "I INSTALLED"
+if (-not (Test-Path $DestinationURL)) {
+    Write-Host -ForegroundColor DarkGray "creating directory @ $HOME\Documents\scripts ..."
+    New-Item -ItemType File -Path $DestinationURL
+}
+
+$response = Invoke-WebRequest -Uri "$SourceURL" -Method Get
+
+Write-Host -ForegroundColor Green "$response"
