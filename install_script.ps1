e19065c9f124eb9373acde69886a76c0923492fa
agp745 <agperez745@gmail.com>
2024-10-01 08:07:57 -0500

    basic install script

---
+++ install_script.ps1
@@ -0,0 +1,8 @@
+# $CLI_URL = ""
+# $DestinationPath = "$HOME\Documents\WindowsPowerShell\Scripts\newHire.ps1"
+#
+# jf (-not (Test-Path $DestinationPath)) {
+#     New-Item -ItemType Directory -Path $DestinationPath
+# }
+
+Write-Host -ForegroundColor Green "I INSTALLED"
