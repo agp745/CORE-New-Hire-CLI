be485e186a58dca86114855a54b30bb8be497e88
agp745 <agperez745@gmail.com>
2024-10-01 10:02:01 -0500

    install now functional

--- install_script.ps1
+++ install_script.ps1
@@ -1,13 +1,33 @@
 $SourceURL = "https://raw.githubusercontent.com/agp745/CORE-New-Hire-CLI/refs/heads/main/newHire.ps1"
-$DestinationDir = "$HOME\Documents\scripts\"
+$DestinationDir = "$HOME\Documents\scripts"
+$File = "$DestinationDir\newHireCLI.ps1"
+
+# =======================
+# Create dir and file
+# =======================
 
 if (-not (Test-Path $DestinationDir)) {
     Write-Host -ForegroundColor DarkGray "creating directory @ $DestinationDir"
     New-Item -ItemType Directory -Path $DestinationDir
 }
 
-New-Item -ItemType File -Path "$DestinationDir\newHireCLI.ps1"
+if (-not (Test-Path $File)) {
+    New-Item -ItemType File -Path $File
+}
+
+# =======================
+# Write script to file
+# =======================
 
 $response = Invoke-WebRequest -Uri "$SourceURL" -Method Get
+$response.Content > $File
 
-Write-Host -ForegroundColor Green "$response"
+# =======================
+# Add alias to $PROFILE
+# =======================
+
+$CLI_Alias = "`nSet-Alias NH $File"
+Add-Content -Path $PROFILE -Value $CLI_Alias
+
+Write-Host -ForegroundColor White "New Hire CLI " -NoNewline
+Write-Host -ForegroundColor Green "installed." 
